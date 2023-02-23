a74b48146f782c07f5efcf7a6130d0b2f716e36c
Ruth Ellen <ruthellen1800@gmail.com>
2023-02-23 15:49:16 -0300

    Adicionar arquivos de projeto.

---
+++ CursoWindowsFormsBiblioteca/Databases/LocalDBClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class LocalDBClass
+    {
+        public string stringConn;
+        public SqlConnection ConnDB;
+
+        public LocalDBClass()
+        {
+            stringConn = "";
+            ConnDB = new SqlConnection(stringConn);
+            ConnDB.Open();
+        }
+    }
+}
