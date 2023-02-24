33b0ac684d3952b3494160a1fef5224142578786
Ruth Ellen <ruthellen1800@gmail.com>
2023-02-24 11:39:22 -0300

    Fichario DB criado

---
+++ CursoWindowsFormsBiblioteca/Databases/FicharioDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class FicharioDB
+    {
+        public string mensagem;
+        public bool status;
+        public string tabela;
+        public LocalDBClass db;
+
+        public FicharioDB(string Tabela)
+        {
+            status = true; 
+            try
+            {
+                db = new LocalDBClass();
+                tabela = Tabela;
+                mensagem = "Conexão com a Tabela criada com sucesso.";
+            }
+            catch (Exception Ex)
+            {
+                status = false;
+                mensagem = "Conexão com a Tabela com erro: " + Ex.Message;
+            }
+        }
+    }
+}
