0bc77181cf504f73e853ad1fa0a62a8bd619bd96
Ruth Ellen <ruthellen1800@gmail.com>
2023-03-02 16:40:38 -0300

    Se conectando ao SQL

---
+++ CursoWindowsFormsBiblioteca/Databases/SQLServerClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class SQLServerClass
+    {
+        public string stringConn;
+        public SqlConnection connDB;
+
+        public SQLServerClass()
+        {
+            try
+            {
+                stringConn = "Data Source=DESKTOP-J5F5202;Initial Catalog=ByteBank;Integrated Security=True";
+                connDB = new SqlConnection(stringConn);
+                connDB.Open();
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception(Ex.Message);
+            }
+        }
+    }
+}
